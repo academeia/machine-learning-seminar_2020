53d037a52142aeda5e3df348e898e7cac11f29c8
Masashi Noda <m-noda@academeia15.co.jp>
2020-04-27 11:59:58 +0900

    fix a bug

--- tf2_time_series/dataset.py
+++ tf2_time_series/dataset.py
@@ -25,7 +25,7 @@ def univariate_data(dataset, start_index, end_index, history_size, target_size):
 
   start_index = start_index + history_size
   if end_index is None:
-  end_index = len(dataset) - target_size
+    end_index = len(dataset) - target_size
 
   for i in range(start_index, end_index):
     indices = range(i-history_size, i)
