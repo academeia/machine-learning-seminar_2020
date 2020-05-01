1b380825e9eb8692452259d4271a6aa25cc705ed
Yusuke Konishi <konishi.yusuke@gmail.com>
2020-05-01 12:58:49 +0900

    add README.md to tf2intro

---
+++ tf2intro/README.md
@@ -0,0 +1,9 @@
+# サンプルコードの実行に際しての注意
+
+以下の順で実行して下さい．
+
+- fmnist.py
+
+- train.py
+
+- predict.py, predict\_single.py (どちらが先でもかまいません)
--- tf2intro/fmnist.py
+++ tf2intro/fmnist.py
@@ -8,8 +8,6 @@ from tensorflow import keras
 import numpy as np
 import matplotlib.pyplot as plt
 
-print(tf.__version__)
-
 fashion_mnist = keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
@@ -22,18 +20,6 @@ print("Train labels:", train_labels)
 
 print("Shape of test images:", test_images.shape)
 print("Test labels:", test_labels)
-
-"""
-plt.figure()
-plt.imshow(train_images[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()
-"""
-
-#train_images = train_images / 255.0
-
-#test_images = test_images / 255.0
 
 plt.figure(figsize=(10,10))
 for i in range(25):
